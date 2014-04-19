b982a8a6f787c0725801f45bfabb4b34383e04e1
zhanghong <zhanghong@networking.io>
2014-04-19 10:42:52 +0800

    修改db/migrate/20140419021807_add_column_app_id_to_tb_app_tokens.rb

--- db/migrate/20140419021807_add_column_app_id_to_tb_app_tokens.rb
+++ db/migrate/20140419021807_add_column_app_id_to_tb_app_tokens.rb
@@ -1,6 +1,5 @@
 class AddColumnAppIdToTbAppTokens < ActiveRecord::Migration
   def change
-    remove_column  :tb_shops,   :app_id
     add_column     :tb_app_tokens,  :app_id,  :integer
   end
 end

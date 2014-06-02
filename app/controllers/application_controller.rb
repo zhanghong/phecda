212c1dcfac232ef2cd6565376d5b5154ea1bb4df
zhanghong <zhanghong@networking.io>
2014-06-02 22:22:31 +0800

    修改登录页面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
-  #before_filter :authenticate_user!
   protect_from_forgery with: :exception
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  # before_filter :set_current_user
-  # layout :layout_by_resource
+  before_filter :set_current_user
+  before_filter :current_shop
+  layout :layout_by_resource
   # authorize_resource :unless => :is_skip_controller?
 
 protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name << :mobile
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name)}
     devise_parameter_sanitizer.for(:account_update) << :name << :mobile
   end
 
@@ -31,12 +31,16 @@ protected
     User.current = current_user
   end
 
+  def current_shop
+    @current_shop ||= (session[:shop_id] && Shop.find(session[:shop_id])) || (current_user && current_user.shops.first)
+    Shop.current = @current_shop
+  end
+
   def layout_by_resource
-    "application"
-    # if devise_controller? && params[:controller] != "registrations"
-    #   "login"
-    # else
-    #   "application"
-    # end
+    if devise_controller? && params[:controller] != "registrations"
+      "login"
+    else
+      "application"
+    end
   end
 end
